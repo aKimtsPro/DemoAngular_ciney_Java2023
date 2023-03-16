f55ddabc3f250b343bf1e68d4b659a4b4b55472e
akimtspro <alexandre.kimtsaris@outlook.com>
2023-03-16 14:16:58 +0100

    demo forms validation

--- src/app/demo/components/demo-forms/demo-forms.component.ts
+++ src/app/demo/components/demo-forms/demo-forms.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Contact } from 'src/app/models/contact.model';
 
 @Component({
   selector: 'app-demo-forms',
@@ -10,18 +11,30 @@ export class DemoFormsComponent {
 
   form: FormGroup;
 
+  contactList: Contact[] = []
+
   constructor(){
     this.form = new FormGroup({
-      'contactName': new FormControl(),
-      'email': new FormControl(),
+      'contactName': new FormControl(''),
+      'email': new FormControl(''),
       'trustIndex': new FormControl(5,[Validators.min(1),Validators.max(10)]),
-      'favorite': new FormControl()
+      'favorite': new FormControl(false)
     })
   }
 
   onSubmit(){
-    // this.form.reset({'trustIndex': 5});
     console.log( this.form );
+    if( this.form.valid ){
+      const contact: Contact = {...this.form.value}
+      this.contactList.push(contact);
+      this.form.reset({
+        'contactName': '',
+        'email': '',
+        'trustIndex': 5,
+        'favorite': false
+      });
+    }
+    
   }
 
 }
